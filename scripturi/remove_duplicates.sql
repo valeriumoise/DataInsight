abc9ebd7f29574546a52c585e4f962cef821afa7
Valeriu <valeriu.moise1010@yahoo.com>
2019-05-20 16:57:39 +0300

    Am facut la delete duplicates sa iti zica daca exista recorduri duplicate dar care sunt referentiate in alte tabele

--- scripturi/remove_duplicates.sql
+++ scripturi/remove_duplicates.sql
@@ -2,10 +2,10 @@ CREATE OR REPLACE TYPE strlist IS
     TABLE OF VARCHAR2(32767);
 /
 
-CREATE OR REPLACE PROCEDURE remove_duplicates (
+CREATE OR REPLACE FUNCTION remove_duplicates (
     table_name    VARCHAR2,
     ignore_cols   IN            strlist DEFAULT strlist('')
-) IS
+) RETURN NUMBER IS
 
     resource_busy EXCEPTION;
     PRAGMA exception_init ( resource_busy, -00054 );
@@ -14,6 +14,7 @@ CREATE OR REPLACE PROCEDURE remove_duplicates (
     v_rec_tab         dbms_sql.desc_tab;
     v_nr_col          NUMBER;
     v_total_coloane   NUMBER;
+    deleted_records   NUMBER;
     v_col_name        VARCHAR2(32767);
     dml_str           VARCHAR2(32767);
     group_by_str      VARCHAR2(32767) := '';
@@ -23,39 +24,56 @@ BEGIN
     v_ok := dbms_sql.execute(v_cursor_id);
     dbms_sql.describe_columns(v_cursor_id, v_total_coloane, v_rec_tab);
     v_nr_col := v_rec_tab.first;
-    dbms_output.put_line(group_by_str);
+--    dbms_output.put_line(group_by_str);
     IF ( v_nr_col IS NOT NULL ) THEN
         LOOP
             v_col_name := v_rec_tab(v_nr_col).col_name;
-            dbms_output.put_line('colname: '
-                                 || v_col_name
-                                 || ', v_nr_col: : '
-                                 || v_nr_col);
+--            dbms_output.put_line('colname: '
+--                                 || v_col_name
+--                                 || ', v_nr_col: : '
+--                                 || v_nr_col);
             IF NOT ( v_col_name MEMBER OF ignore_cols ) THEN
                 IF ( v_nr_col > 1 ) THEN
                     group_by_str := concat(concat(group_by_str, v_col_name), ', ');
                 ELSE
                     group_by_str := v_col_name || ', ';
                 END IF;
+
             END IF;
 
-            dbms_output.put_line(group_by_str);
+--            dbms_output.put_line(group_by_str);
+
             v_nr_col := v_rec_tab.next(v_nr_col);
             EXIT WHEN ( v_nr_col IS NULL );
         END LOOP;
     END IF;
 
     group_by_str := substr(group_by_str, 1, length(group_by_str) - 2);
-    dbms_output.put_line(group_by_str);
+--    dbms_output.put_line(group_by_str);
     dml_str := 'DELETE FROM '
                || table_name
-               || ' WHERE rowid NOT IN ( SELECT MAX(ROWID) FROM '
+               || ' WHERE rowid NOT IN ( SELECT min(ROWID) FROM '
                || table_name
                || ' GROUP BY '
                || group_by_str
                || ' )';
-
-    dbms_output.put_line(dml_str);
     EXECUTE IMMEDIATE dml_str;
+    
+    deleted_records := SQL%rowcount;
     dbms_sql.close_cursor(v_cursor_id);
+    
+
+    RETURN deleted_records;    
+End;
+EXCEPTION
+   WHEN OTHERS
+   THEN
+   DECLARE
+      error_code NUMBER := SQLCODE;
+   BEGIN
+        IF error_code = -2292
+        THEN
+            dbms_output.put_line('Found duplicates but both have references. Use merge_duplicates() merge records.');
+        END IF;
+    END;
 END;
